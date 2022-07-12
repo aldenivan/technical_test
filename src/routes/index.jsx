c37469fcc44a96a5e8bcb9232f0d831883812ce3
Aldenivan Jr <aldenivan1207@gmail.com>
2022-07-12 12:21:46 -0300

    feat: fixing some bugs page Login and create pege register

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,9 +1,11 @@
 import { Switch } from "react-router-dom";
 import { Login } from "../pages/Login";
+import { SignUp } from "../pages/SignUp";
 import { Route } from "./Route";
 
 export const Routes = () => (
   <Switch>
     <Route exact path="/" Component={Login} />
+    <Route path="/signup" Component={SignUp} />
   </Switch>
 );
