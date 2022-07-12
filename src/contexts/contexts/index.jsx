dc940bc28a0e9bdcbe598967b10f8bbe8aba3c66
Aldenivan Jr <aldenivan1207@gmail.com>
2022-07-12 13:24:56 -0300

    feat: creating dashboard page

--- src/contexts/contexts/index.jsx
+++
@@ -1,9 +0,0 @@
-import { ChakraProvider } from "@chakra-ui/react";
-import { theme } from "../styles/theme";
-import { AuthProvider } from "./AuthContext";
-
-export const AppProvider = ({ children }) => (
-  <AuthProvider>
-    <ChakraProvider theme={theme}>{children}</ChakraProvider>
-  </AuthProvider>
-);
